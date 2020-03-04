18560fe2717dfb2d00dbc181fe4ab8c4b274095e
Adriano Caetano <adrianu_f22@hotmail.com>
2020-03-04 03:22:23 -0300

    Add Symfony e Bref

---
+++ src/Controller/UserController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController
+{
+    public function add() : Response
+    {
+        return new Response();
+    }
+}
